4c84a0141cacc1fafd2246087aa7ecf1a204d8b4
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2025-08-15 14:55:15 +0330

    now we can shoot

--- app/routes/lobby.tsx
+++ app/routes/lobby.tsx
@@ -4,19 +4,29 @@ import {
   useThree,
   type ThreeEvent,
 } from "@react-three/fiber";
-import { forwardRef, useRef } from "react";
+import { useRef, useState } from "react";
 import { OrbitControls } from "@react-three/drei";
 import * as THREE from "three";
 import { type Ref } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 type BoxProps = {
   ref: Ref<THREE.Mesh>;
-  handleClickBox: (event: ThreeEvent<MouseEvent>) => void;
+  shootingTheBullet: (
+    event: ThreeEvent<MouseEvent>,
+    args: [number, number, number],
+    color: string
+  ) => void;
 };
 
-function Box({ handleClickBox, ref }: BoxProps) {
+function Box({ shootingTheBullet, ref }: BoxProps) {
   return (
-    <mesh ref={ref} onClick={handleClickBox} position={[0, 0, 1]} scale={1}>
+    <mesh
+      ref={ref}
+      onClick={(e) => shootingTheBullet(e, [1, 1, 1], "red")}
+      position={[0, 0, 1]}
+      scale={1}
+    >
       <boxGeometry args={[5, 5, 1]} />
       <meshStandardMaterial color={"blue"} />
     </mesh>
@@ -24,15 +34,15 @@ function Box({ handleClickBox, ref }: BoxProps) {
 }
 
 type SurfaceProps = {
-  handlePointerMove: (event: ThreeEvent<PointerEvent>) => void;
+  moveTheShip: (event: ThreeEvent<PointerEvent>) => void;
 };
 
-export function Surface({ handlePointerMove }: SurfaceProps) {
+export function Surface({ moveTheShip }: SurfaceProps) {
   const { viewport } = useThree();
 
   return (
     <mesh
-      onPointerMove={handlePointerMove}
+      onPointerMove={moveTheShip}
       position={[0, 0, 0]}
       scale={[viewport.width, viewport.height, 1]}
     >
@@ -42,7 +52,42 @@ export function Surface({ handlePointerMove }: SurfaceProps) {
   );
 }
 
+interface Bullet {
+  position: [number, number, 1];
+  args: [number, number, number];
+  color: string;
+  id: string;
+}
+
+type BulletProps = {
+  bullet: Bullet;
+  removeOutOfRangeBullets: (id: string) => void;
+};
+
+function Bullet({ bullet, removeOutOfRangeBullets }: BulletProps) {
+  const bulletRef = useRef<THREE.Mesh>(null!);
+  const { viewport } = useThree();
+  const bulletHalfHeight = bullet.args[1] / 2;
+
+  useFrame((_, delta) => {
+    bulletRef.current.position.y += 50 * delta;
+
+    if (bulletRef.current.position.y > viewport.height / 2 + bulletHalfHeight) {
+      removeOutOfRangeBullets(bullet.id);
+    }
+  });
+
+  return (
+    <mesh ref={bulletRef} position={bullet.position}>
+      <boxGeometry args={bullet.args} />
+      <meshStandardMaterial color={bullet.color} />
+    </mesh>
+  );
+}
+
 function Scene() {
+  const [bullets, setBullets] = useState<Bullet[]>([]);
+
   const boxRef = useRef<THREE.Mesh>(null!);
   const mousePos = useRef({ x: 0, y: 0 });
 
@@ -51,8 +96,26 @@ function Scene() {
     mousePos.current = { x: event.point.x, y: event.point.y };
   }
 
-  function handleClickBox(event: ThreeEvent<MouseEvent>) {
-    console.log("hello there");
+  function handleClickBox(
+    event: ThreeEvent<MouseEvent>,
+    args: [number, number, number],
+    color: string
+  ) {
+    setBullets((bullets) => [
+      ...bullets,
+      {
+        position: [event.point.x, event.point.y, 1],
+        args,
+        color,
+        id: uuidv4(),
+      },
+    ]);
+  }
+
+  function handleDeleteBullet(id: string) {
+    setBullets((currentBullets) =>
+      currentBullets.filter((bullet) => bullet.id !== id)
+    );
   }
 
   useFrame(() => {
@@ -66,8 +129,17 @@ function Scene() {
     <>
       <OrbitControls />
       <ambientLight intensity={Math.PI / 2} />
-      <Box ref={boxRef} handleClickBox={handleClickBox} />
-      <Surface handlePointerMove={handlePointerMove} />
+      <Box ref={boxRef} shootingTheBullet={handleClickBox} />
+      <Surface moveTheShip={handlePointerMove} />
+      {bullets.map((bullet) => {
+        return (
+          <Bullet
+            key={bullet.id}
+            bullet={bullet}
+            removeOutOfRangeBullets={handleDeleteBullet}
+          />
+        );
+      })}
     </>
   );
 }
